1041774a3c6530f2b1ced8d5f5b445e68ada6264
mr-bones <mr-bones@debian.debian.local>
2026-01-27 19:47:54 -0700

    Plan: Pseudocode for analyser

--- analyse.py
+++ analyse.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
 # Load data from data file
-dataf = pd.read_csv('res/data.csv')
+df = pd.read_csv('res/data.csv')
+
+# Fill missing values with mean
+df.fillna(df.mean(), inplace=True)
+
+# Calculate average macronutrient content
+avg_macros = df.groupby('Diet_Type')[['Protein(g)', 'Carbs(g)', 'Fat(g)']].mean()
+
+# Get top 5 recipes by protein-richness
+top_prot = df.sort_values('Protein(g)', ascending=False).groupby('Diet_Type').head(5)
+
+# Calculate protein-to-carb and carb-to-fat ratios
+df['Protein_to_Carbs'] = df['Protein(g)'] / df['Carbs(g)']
+df['Carbs_to_Fat'] = df['Carbs(g)'] / df['Fat(g)']
+
+# Test
+print(df.head(10))
