57d02028d9d2eb147ceb19818c8aef81dc77e1da
mr-bones <mr-bones@debian.debian.local>
2026-01-27 20:05:57 -0700

    Fix: Only find mean of numbers and use correct column name

--- analyse.py
+++ analyse.py
@@ -4,13 +4,13 @@ import pandas as pd
 df = pd.read_csv('res/data.csv')
 
 # Fill missing values with mean
-df.fillna(df.mean(), inplace=True)
+df.fillna(df.mean(numeric_only=True), inplace=True)
 
 # Calculate average macronutrient content
-avg_macros = df.groupby('Diet_Type')[['Protein(g)', 'Carbs(g)', 'Fat(g)']].mean()
+avg_macros = df.groupby('Diet_type')[['Protein(g)', 'Carbs(g)', 'Fat(g)']].mean()
 
 # Get top 5 recipes by protein-richness
-top_prot = df.sort_values('Protein(g)', ascending=False).groupby('Diet_Type').head(5)
+top_prot = df.sort_values('Protein(g)', ascending=False).groupby('Diet_type').head(5)
 
 # Calculate protein-to-carb and carb-to-fat ratios
 df['Protein_to_Carbs'] = df['Protein(g)'] / df['Carbs(g)']
