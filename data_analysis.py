77b101d8d01438f8672ffcc2216dfd4ead49bf17
TerrellAW <terraw1999@proton.me>
2026-01-28 15:53:28 -0700

    Fix: Remove unexpected spaces

--- data_analysis.py
+++ data_analysis.py
@@ -6,20 +6,20 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('res/data.csv')
 
 # Fill missing values with mean
-df.fillna(df.mean(numeric_only = True), inplace = True)
+df.fillna(df.mean(numeric_only=True), inplace=True)
 
 # Calculate average macronutrient content
 avg_macros = df.groupby('Diet_type')[['Protein(g)', 'Carbs(g)', 'Fat(g)']].mean()
 
 # Get top 5 recipes by protein-richness
-top_prot = df.sort_values('Protein(g)', ascending = False).groupby('Diet_type').head(5)
+top_prot = df.sort_values('Protein(g)', ascending=False).groupby('Diet_type').head(5)
 
 # Calculate protein-to-carb and carb-to-fat ratios
 df['Protein_to_Carbs'] = df['Protein(g)'] / df['Carbs(g)']
 df['Carbs_to_Fat'] = df['Carbs(g)'] / df['Fat(g)']
 
 # Average macronutrients bar chart
-sbn.barplot(x = avg_macros.index, y = avg_macros['Protein(g)'])
+sbn.barplot(x=avg_macros.index, y=avg_macros['Protein(g)'])
 plt.title('Average Protein by Diet Type')
 plt.ylabel('Average Protein (g)')
 plt.show()
